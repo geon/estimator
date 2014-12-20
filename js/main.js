0f2166ab7287c644177aa075492875b613389963
Victor Widell <victor@topmost.se>
2014-12-20 16:46:43 +0100

    Hardcoded HTML => data + template.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,89 @@
 $(function () {
 
-	$('.tree-view ul li' ).draggable({
 
-		// handle: '.handle',
-		zIndex: 100
-	});
+	var $template = $($.parseHTML($('script.js-task[type=template]').text()));
+	var $treeView = $('.tree-view');
 
-	$('.task .drop-target').droppable({
 
-		tolerance: 'pointer',
-
-		over: function ( event, ui ) {
-
-			console.log('over', event, ui);
-
-			$(this).toggleClass('drop-hover', true);
+	$.ajax({
+		url: 'task.json',
+		dataType: 'json',
+		error: function () {
+			console.log('error getting task');
 		},
+		success: function (task) {
 
-		out: function ( event ) {
+			$treeView.find('ul').append(task.tasks.map(makeLi));
 
-			console.log('out', event);
+			function makeLi(task) {
 
-			$(this).toggleClass('drop-hover', false);
-		},
+				var li = $template.clone();
 
-		drop: function( event, ui ) {
+				li.find('h1').text(task.title).end();
+				li.find('.task').addClass(task.color);
 
-			console.log('drop', event, ui);
+				li.find('ul').append(task.tasks && task.tasks.map(makeLi));
 
-			// Stop indicating drop target.
-			$(this).toggleClass('drop-hover', false);
+				return li;
+			}
 
-			// Place dragged after drop target.
-			var li = $(this).closest('li');
-			li.after(ui.draggable);
-			ui.draggable.css({
-				left: 0,
-				top: 0
-			});
 
-			// Re-enable draggable.
-			li.draggable({
+
+			$('.tree-view ul li' ).draggable({
 
 				// handle: '.handle',
 				zIndex: 100
 			});
+
+			$('.task .drop-target').droppable({
+
+				tolerance: 'pointer',
+
+				over: function ( event, ui ) {
+
+					console.log('over', event, ui);
+
+					$(this).toggleClass('drop-hover', true);
+				},
+
+				out: function ( event ) {
+
+					console.log('out', event);
+
+					$(this).toggleClass('drop-hover', false);
+				},
+
+				drop: function( event, ui ) {
+
+					console.log('drop', event, ui);
+
+					// Stop indicating drop target.
+					$(this).toggleClass('drop-hover', false);
+
+					// Place dragged after drop target.
+					var li = $(this).closest('li');
+					li.after(ui.draggable);
+					ui.draggable.css({
+						left: 0,
+						top: 0
+					});
+
+					// Re-enable draggable.
+					li.draggable({
+
+						// handle: '.handle',
+						zIndex: 100
+					});
+				}
+			});
+
+
 		}
 	});
+
+
+
+
+
 
 });
