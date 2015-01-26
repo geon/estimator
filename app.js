c70197492efd9887e49ebe73e337b85d348fb105
Victor Widell <victor@topmost.se>
2015-01-26 21:56:30 +0100

    Added method list to CORS headers.

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ apiRouter.use(function(req, res, next) {
 
     // CORS-headers.
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
