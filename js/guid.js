c3d6636950a8304adb45888ebdac4d38e518515b
Victor Widell <victor.widell@divid.se>
2017-12-18 09:08:12 +0100

    Removed live demo.

--- js/guid.js
+++
@@ -1,9 +0,0 @@
-// http://stackoverflow.com/a/2117523/446536
-
-function makeGuid () {
-
-	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-		var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-		return v.toString(16);
-	});
-};
